58fa88aec1e21fb0876c997559e5be1b35f9da26
First name Last name <58491627+Sukran1981@users.noreply.github.com>
2020-04-05 16:23:32 -0400

    new project

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -3,17 +3,28 @@ package com.weborders.pages;
 
 import com.weborders.utilities.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-@FindBy
 
 public abstract class AbstractBasePage {
+
     protected WebDriver driver=Driver.getDriver();
     protected WebDriverWait wait=new WebDriverWait(driver,20);
 
+
+    @FindBy(tagName = "h1")
+    protected WebElement pageLogo;
+
+    public String getPageLogoText(){
+
+        return pageLogo.getText();
+    }
+
     public AbstractBasePage(){//OBJECT CREATED WITH CONSTRUCTOR MAKE IT RUNNABLE EXECUTED RUNNING
+
         PageFactory.initElements(Driver.getDriver(),this);//just for @Find annotaion
     }
 }
